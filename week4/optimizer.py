9ec6d443ffb2243a827f6a4188eb76bbda2b4e0e
9and3 <andreasettimi39@gmail.com>
2022-10-18 15:34:40 +0200

    CAP: added conda environment and basic python structure

---
+++ week4/optimizer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from typing import Callable
+
+
+
+def optimizer(s: Callable)-> float:
+    pass
+
+
+def main()-> int:
+
+
+    return 0;
+
+
+
+
+if __name__ == "__main__":
+    if main() != 0:
+        sys.exit(1)
