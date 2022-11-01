118a78f8b63c86f5b957fe253df94dbf32ac6f5a
9and3 <andreasettimi39@gmail.com>
2022-11-01 14:40:22 +0100

    ADD: doxygen for function compute

--- homework2/src/series.hh
+++ homework2/src/series.hh
@@ -5,6 +5,9 @@ namespace SCPP
     class Series
     {
     public:
+        /// @brief compute the Nth term of the series
+        /// @param N number of terms
+        /// @return the result of the series as double
         virtual double compute(unsigned int N) = 0;
     };
 }
