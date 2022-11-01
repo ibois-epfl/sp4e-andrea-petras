18a5251dceb67d0b3e32c891d899698c37928041
Petras Vestartas <petras.vestartas@epfl.ch>
2022-11-01 11:08:06 -0700

    ADD: new function SetSeparator()

--- homework2/src/write_series.hh
+++ homework2/src/write_series.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include "series.hh"
 #include "dumper_series.hh"
+#include <string>
 
 namespace SCPP
 {
@@ -15,9 +16,16 @@ namespace SCPP
 
         /// @brief output (to a file) every step out of frequency that is lower than maxiter.
         virtual void dump() override;
+
+        /// @brief change file ending 
+        /// @param separator: ',' = .csv, ' ' = .txt, '\t' = .txt, '|' = .psv,
+        void SetSeparator(char separator);
     
     public:
         double Frequency;
         double Maxiter;
+    
+    private:
+        std::string m_Ending;
     };
 }
