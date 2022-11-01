bbc33592a2636a0f6eb13bb760df33d29168ee7c
9and3 <andreasettimi39@gmail.com>
2022-11-01 17:45:43 +0100

    FIX: precision function + member

--- homework2/src/dumper_series.hh
+++ homework2/src/dumper_series.hh
@@ -7,15 +7,16 @@ namespace SCPP
     {
     public:
         DumperSeries(Series& series)
-            : p_Series(series)
+            : p_Series(series), p_Precision(4)
         {};
         ~DumperSeries() = default;
 
         virtual void dump(std::ostream& os) = 0;
 
-        virtual void setPrecision(unsigned int precision);
+        virtual void setPrecision(unsigned int precision) {p_Precision = precision; };
 
     protected:
         Series& p_Series;
+        unsigned int p_Precision;
     };
 }
