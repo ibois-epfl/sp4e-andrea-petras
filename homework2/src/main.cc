8ab717c9c61b62bb2b6cce25e8e0c5897dc26fbe
9and3 <andreasettimi39@gmail.com>
2022-11-01 10:54:06 +0100

    MILESTONE:EXO2.5:from PETRAS: testing the created child pi class

--- homework2/src/main.cc
+++ homework2/src/main.cc
@@ -3,12 +3,17 @@
 
 #include "series.hh"
 #include "compute_arithmetic.hh"
+#include "compute_pi.hh"
 
 int main(int argc, char** argv)
 {
     // Exo 2.3-4
     SCPP::ComputeArithmetic ca;
     std::cout << ca.compute(10) << std::endl;
+
+    // Exo 2.5
+    SCPP::ComputePi cp;
+    std::cout << cp.compute(10) << std::endl;
 
     return 0;
 }
