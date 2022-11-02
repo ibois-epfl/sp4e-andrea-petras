f826e408110877d7f80b9601d8495c46c8ba51d6
Andrea Settimi <andreasettimi39@gmail.com>
2022-11-02 12:25:51 +0100

    ADD: members for avoiding re-computing

--- homework2/src/series.hh
+++ homework2/src/series.hh
@@ -10,9 +10,14 @@ namespace SCPP
         /// @brief compute the Nth term of the series
         /// @param N number of terms
         /// @return the result of the series as double
+        // virtual double compute(unsigned int N) = 0;
         virtual double compute(unsigned int N) = 0;
 
         /// @brief provides the analytic prediction of a class, by default returns NaN
         virtual double getAnalyticPrediction() { return std::nan("0"); };
+
+    public:
+        unsigned int CurrentIndex = 0;
+        double CurrentValue = 0.0;
     };
 }
