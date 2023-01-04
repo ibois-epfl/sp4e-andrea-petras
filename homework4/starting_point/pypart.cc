5aed320ac2317b9eaf24a3eb6b37ef7ac8257214
Andrea Settimi <andreasettimi39@gmail.com>
2023-01-04 13:51:27 +0100

    ADD: compute wraps

--- homework4/starting_point/pypart.cc
+++ homework4/starting_point/pypart.cc
@@ -9,6 +9,7 @@ namespace py = pybind11;
 #include "ping_pong_balls_factory.hh"
 #include "planets_factory.hh"
 
+#include "system_evolution.hh"
 #include "compute.hh"
 #include "compute_temperature.hh"
 #include "compute_gravity.hh"
@@ -28,12 +29,12 @@ PYBIND11_MODULE(pypart, m) {
 
   m.doc() = "pybind of the Particles project";
 
-  // bind the routines here
+  // main objects
   py::class_<ParticlesFactoryInterface>(
       m, "ParticlesFactoryInterface",
       py::dynamic_attr()
       )
-      .def("getInstance", &ParticlesFactoryInterface::getInstance, py::return_value_policy::reference)
+      .def("getInstance", &ParticlesFactoryInterface::getInstance, py::return_value_policy::reference);
 
   py::class_<MaterialPointsFactory, ParticlesFactoryInterface>(
       m, "MaterialPointsFactory",
@@ -54,10 +55,48 @@ PYBIND11_MODULE(pypart, m) {
       .def("getInstance", &PlanetsFactory::getInstance, py::return_value_policy::reference);
 
 
-  // py::class_<ComputeTemperature, Compute, std::shared_ptr<ComputeTemperature>>(
-  //     m, "ComputeTemperature",
-  //     py::dynamic_attr()
-  //     )
-  //     .def(py::init<>());
+  // compute objects/methods
+  py::class_<SystemEvolution>(
+      m, "SystemEvolution",
+      py::dynamic_attr()
+      )
+      .def("addCompute", &SystemEvolution::addCompute, py::arg("compute"));
+
+  py::class_<Compute, std::shared_ptr<Compute>>(m, "Compute");
+
+  py::class_<ComputeTemperature, Compute, std::shared_ptr<ComputeTemperature>>(
+      m, "ComputeTemperature",
+      py::dynamic_attr()
+      )
+      .def(py::init<>())
+      /* here we could have modified the .hh and add setters (i.e. setConductivity()):
+      *     def_property("conductivity", &ComputeTemperature::getConductivity, &ComputeTemperature::setConductivity)
+      *  but we can also use the def_property() method to set the private variables.
+      */
+      .def_property("conductivity", &ComputeTemperature::getConductivity, 
+          [](ComputeTemperature& self, Real value) { self.getConductivity() = value; })
+      .def_property("capacity", &ComputeTemperature::getCapacity, 
+          [](ComputeTemperature& self, Real value) { self.getCapacity() = value; })
+      .def_property("density", &ComputeTemperature::getDensity, 
+          [](ComputeTemperature& self, Real value) { self.getDensity() = value; })
+      .def_property("L", &ComputeTemperature::getL, 
+          [](ComputeTemperature& self, Real value) { self.getL() = value; })
+      .def_property("deltat", &ComputeTemperature::getDeltat, 
+          [](ComputeTemperature& self, Real value) { self.getDeltat() = value; });
+  
+  py::class_<ComputeGravity, Compute, std::shared_ptr<ComputeGravity>>(
+      m, "ComputeGravity",
+      py::dynamic_attr()
+      )
+      .def(py::init<>())
+      .def("setG", &ComputeGravity::setG);
+
+  py::class_<ComputeVerletIntegration, Compute, std::shared_ptr<ComputeVerletIntegration>>(
+      m, "ComputeVerletIntegration",
+      py::dynamic_attr()
+      )
+      .def(py::init<Real>())
+      .def("addInteraction", &ComputeVerletIntegration::addInteraction);
+
 
 }
